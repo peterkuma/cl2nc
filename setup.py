cfa72273f223afe09015a02f301b75cfaf7564af
Peter Kuma <peter.kuma@pg.canterbury.ac.nz>
2017-09-26 16:55:03 +1300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='cl2nc',
-    version='1.0',
+    version='1.0.1',
     description='Convert Vaisala CL51 and CL31 dat files to NetCDF',
     author='Peter Kuma',
     author_email='peter.kuma@fastmail.com',
     license='MIT',
     scripts=['cl2nc'],
     install_requires=['netCDF4>=1.2.9'],
-    package_data={
-        '': ['*.md'],
-    }
+    keywords=['vaisala', 'ceilometer', 'cl51', 'cl31', 'netcdf'],
+    url='https://github.com/peterkuma/cl2nc',
 )
